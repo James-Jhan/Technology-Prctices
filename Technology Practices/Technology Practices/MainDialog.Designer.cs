cdf78772070185de2d17bf6e35deb8758f37a389
James <james920x@gmail.com>
2017-11-21 23:39:21 +0800

    add MainDialog, and link to Chapter1 page.

---
+++ Technology Practices/Technology Practices/MainDialog.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Technology_Practices
+{
+    partial class MainDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Chapter1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Chapter1
+            // 
+            this.Chapter1.Location = new System.Drawing.Point(69, 48);
+            this.Chapter1.Name = "Chapter1";
+            this.Chapter1.Size = new System.Drawing.Size(94, 47);
+            this.Chapter1.TabIndex = 0;
+            this.Chapter1.Text = "Chapter 1";
+            this.Chapter1.UseVisualStyleBackColor = true;
+            this.Chapter1.Click += new System.EventHandler(this.Chapter1_Click);
+            // 
+            // MainDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 381);
+            this.Controls.Add(this.Chapter1);
+            this.Name = "MainDialog";
+            this.Text = "MainDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Chapter1;
+    }
+}
---
+++ Technology Practices/Technology Practices/MainDialog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Technology_Practices
+{
+    public partial class MainDialog : Form
+    {
+        public MainDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Chapter1_Click(object sender, EventArgs e)
+        {
+            Chapter1 dlg = new Chapter1();
+
+            this.Visible = false;
+
+            dlg.Visible = true;
+        }
+    }
+}
